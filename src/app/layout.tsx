be686d2b10a9a454616bcc38ceeba02a508e15f5
Justin Charles <charlesjustin2124@gmail.com>
2025-09-19 19:47:47 +0530

    change font style in heading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300;400;700&display=swap" rel="stylesheet" />
+      </head>
       <body className="antialiased">
         <ErrorReporter />
         <Script
