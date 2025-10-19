3882a670c9621f30779c55a0168722a821290222
M-DEV-1 <mahadevankizhakkedathu@gmail.com>
2025-10-19 21:55:44 +0530

    update
    Signed-off-by: M-DEV-1 <mahadevankizhakkedathu@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import VisualEditsMessenger from "../visual-edits/VisualEditsMessenger";
 import "./globals.css";
 import ErrorReporter from "@/components/ErrorReporter";
 import Script from "next/script";
@@ -23,17 +22,7 @@ export default function RootLayout({
       </head>
       <body className="antialiased">
         <ErrorReporter />
-        <Script
-          src="https://slelguoygbfzlpylpxfs.supabase.co/storage/v1/object/public/scripts//route-messenger.js"
-          strategy="afterInteractive"
-          data-target-origin="*"
-          data-message-type="ROUTE_CHANGE"
-          data-include-search-params="true"
-          data-only-in-iframe="true"
-          data-debug="true"
-          data-custom-data='{"appName": "YourApp", "version": "1.0.0", "greeting": "hi"}'
-        />{children}
-  <VisualEditsMessenger />
+        {children}
 </body>
     </html>
   );
